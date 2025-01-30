cbf9f8ccce9e5ecb41d4ee15eed068f20d52303d
carana08 <carana@espol.edu.ec>
2025-01-29 19:46:34 -0500

    Arreglo de eliminación de comunidad

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,19 +1,18 @@
 
 
 class Community < ApplicationRecord
-  
   belongs_to :user
 
-  #has_many :users, dependent: :destroy
+  # has_many :users, dependent: :destroy
   has_many :events, dependent: :destroy
-  has_many :posts, dependent: :destroy 
+  has_many :posts, dependent: :destroy
 
-   # agregado César
-  has_many :polls  
+  # agregado César
+  has_many :polls, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true 
-  
+  validates :description, presence: true
+
   # AGREGADO LUIS INGA
   # Devuelve el número total de eventos creados en la comunidad
   def events_count
